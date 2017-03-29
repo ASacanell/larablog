3c1598aa94fee98fd6affc0d8c539819323f52b0
ASacanell <alejandro.sacanell.developer@gmail.com>
2017-03-28 18:48:15 -0700

    add link to /dashboard for dashboard views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,6 +46,9 @@
                     <a class="navbar-brand" href="{{ url('/') }}">
                         {{ config('app.name', 'Blog') }}
                     </a>
+                    <a class="navbar-brand" href="{{ url('/dashboard') }}">
+                        Dashboard
+                    </a>
                 </div>
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
